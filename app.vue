1aaee1a258e473caaa76d9e07a04c590194a836f
Tito Riskimianto <riskimianto@gmail.com>
2023-05-25 15:02:51 +0700

    initial commit

---
+++ app.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container">
+    <NuxtLayout :name="comingsoon"></NuxtLayout>
+  </div>
+</template>
+
+<style>
+  .container{
+    max-width: 100%;
+  }
+</style>
+
+<script setup>
+  const comingsoon = "comingsoon";
+</script>
---
+++ layouts/comingsoon.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="flex items-center justify-center h-screen bg-[#FF5A5F]">
+        <div class="text-center">
+            <img class="mx-auto mb-8" src="/icons/airbnb.png" alt="airbnb">
+            <h1 class="text-white text-4xl font-bold mb-4">Coming Soon</h1>
+            <p class="text-gray-100">Airbnb project by Tito Riskimianto</p>            
+        </div>
+    </div>
+</template>
