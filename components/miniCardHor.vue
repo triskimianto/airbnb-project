a9f1fcd619e806c9e4ddda63d513eeb476ef2d46
Tito Riskimianto <riskimianto@gmail.com>
2023-05-26 13:43:05 +0700

    add components

---
+++ components/miniCardHor.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="mx-20 my-10">
+        <div class="flex flex-row w-[391px] h-[85px] bg-white shadow-lg rounded-[12px] p-8 md:p-0">
+            <img class="w-[121px] rounded-l-[12px] " src="/Rectangle 4.png" alt="" width="384" height="512">
+            <div class="px-10 ">
+                <div class="mt-[33px]">
+                    <p class="text-medium text['#484848'] font-medium">
+                        Logements
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
---
+++ components/miniCardVer.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="w-[188px]">
+        <img class="h-[240px] rounded-[4px]" src="/Rectangle 4.png" alt="">
+        <div class="my-[8px]">
+            <h1 class="text-[10px] text-start text-[#767676] font-bold uppercase">Country</h1>
+            <h1 class="text-[16px] text-start text-[#484848] font-bold mt-[4px]">2 Nights PACKAGE All Inclusive</h1>
+        </div>
+        <h1 class="text-[14px] text-start text-[#484848] font-light">À partir de 577€/personne - 3 jours</h1>        
+        <div class="flex flex-row mt-[5px] items-center">
+            <h1 class="text-[12px] text-start text-[#008489] font-bold">5.0</h1>
+            <img class="w-[8px] h-[8px] ml-[4px]" src="/icons/star.png" alt="">
+        </div>
+    </div>
+</template>
